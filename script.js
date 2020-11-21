8d895228548dbbc0b5bd20b98b8f8f62fd5fd192
Tracy Golden <tracy80s2003@gmail.com>
2020-11-21 13:17:52 -0600

    renderTodos

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
 
 var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+
+function renderTodos () {
+    for (var i = 0; i < todos.length; i++) {
+        // create li element
+        var li = document.createElement('li')
+        // add todo text to li
+        li.innerText = todos[i]
+        // append li to todoList
+        todoList.appendChild(li)
+    }
+}
+
+renderTodos ()
